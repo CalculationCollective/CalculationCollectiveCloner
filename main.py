2092fd70bc71a8c8a6b67a247fd5ea1cdc624891
Teddy <teddy@teddy.email>
2023-02-15 21:47:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+import os
+
+response = requests.get("https://api.github.com/orgs/CalculationCollective/repos?per_page=1000")
+if not response.ok:
+    print(f"github api returned with code {response.status_code}")
+
+if not os.path.isdir("CalculationCollective/"):
+    os.mkdir("CalculationCollective/")
+
+for repo in response.json():
+    os.system(f"git -C CalculationCollective/ clone {repo['clone_url']}")
