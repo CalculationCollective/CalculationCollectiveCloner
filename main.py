715fd209e02f332cf3cf63ecd3660f607aa28362
Teddy55 <103327059+Teddy55Codes@users.noreply.github.com>
2023-04-03 22:25:16 +0200

    now exiting if github api request failed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 response = requests.get("https://api.github.com/orgs/CalculationCollective/repos?per_page=1000")
 if not response.ok:
     print(f"github api returned with code {response.status_code}")
+    exit()
 
 if not os.path.isdir("CalculationCollective/"):
     os.mkdir("CalculationCollective/")
